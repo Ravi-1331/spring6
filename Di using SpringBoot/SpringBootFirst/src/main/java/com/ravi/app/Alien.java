b025d88a5bdbaf3c95822152ae5fc54f180333d9
Ravi-1331 <ravimuzpatna44@gmail.com>
2026-09-20 15:22:42 +0530

    Add 1.8 DI Using Spring Boot project

---
+++ Di using SpringBoot/SpringBootFirst/src/main/java/com/ravi/app/Alien.java
@@ -0,0 +1,10 @@
+package com.ravi.app;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Alien {
+    public void code(){
+        System.out.println("Coding...");
+    }
+}
---
+++ Di using SpringBoot/SpringBootFirst/src/main/java/com/ravi/app/SpringBootFirstApplication.java
@@ -0,0 +1,17 @@
+package com.ravi.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringBootFirstApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(SpringBootFirstApplication.class, args);
+
+		Alien obj = context.getBean(Alien.class);
+		obj.code();
+	}
+
+}
