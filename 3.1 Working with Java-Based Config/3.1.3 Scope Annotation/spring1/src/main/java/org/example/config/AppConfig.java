bcb586d28997cfcef674cc832f65b1431fafc3c0
Ravi-1331 <ravimuzpatna44@gmail.com>
2026-09-22 16:02:16 +0530

    Add project

---
+++ 3.1 Working with Java-Based Config/3.1.3 Scope Annotation/spring1/src/main/java/org/example/config/AppConfig.java
@@ -0,0 +1,17 @@
+package org.example.config;
+
+import org.example.Desktop;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class AppConfig {
+
+    // @Bean(name = {"com2", "Beast", "desktop1"})  //bean nameing
+    @Bean
+    @Scope("prototype")
+    public Desktop desktop(){
+        return new Desktop();
+    }
+}
