ddb911eb04f75795bd6f3a9f7386b87b600f7d41
Ravi-1331 <ravimuzpatna44@gmail.com>
2026-09-22 17:01:56 +0530

    Add project

---
+++ 3.1 Working with Java-Based Config/3.1.8 Primary Annotation/spring1/src/main/java/org/example/Desktop.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+//@Component("com1")
+@Component
+@Primary
+public class Desktop implements Computer {
+    public Desktop(){
+        System.out.println("Desktop object created");
+    }
+    @Override
+    public void compile(){
+        System.out.println("Compiling using Desktop");
+    }
+}
